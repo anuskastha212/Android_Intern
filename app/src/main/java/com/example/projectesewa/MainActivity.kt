5e03845d943b1ba6581ddf854d7910da5f68b671
Anuska <anuskastha212@gmail.com>
2026-07-09 13:36:00 +0545

    Added search bar

--- app/src/main/java/com/example/projectesewa/MainActivity.kt
+++ app/src/main/java/com/example/projectesewa/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.example.projectesewa
 
 import android.os.Bundle
+import android.text.Html
+import android.widget.TextView
+import androidx.core.text.HtmlCompat
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,6 +15,15 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+
+        val john = findViewById<TextView>(R.id.john)
+
+        john.text = HtmlCompat.fromHtml(
+            getString(R.string.John),
+            HtmlCompat.FROM_HTML_MODE_LEGACY
+        )
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
